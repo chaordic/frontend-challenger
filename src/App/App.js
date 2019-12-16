627c4062581bc326494fecfc09773893e152395c
Carlos <am53_coliveira@uolinc.com>
2019-12-16 16:48:34 -0300

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const App = () => <h1> Begin</h1>;
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+
+const initialState = {};
+
+const redux = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ redux });
