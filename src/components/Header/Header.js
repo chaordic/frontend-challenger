f08bebc1d84be565419ef128442dd61f75afe977
Carlos <am53_coliveira@uolinc.com>
2019-12-17 17:41:24 -0300

    [WIP] Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import styles from "./Header.scss";
 
-export const Header = () => (
+export const Header = ({ id, status, fulfillments }) => (
   <header className={styles.header}>
-    <div className={styles.container}>Tratamento de entregas</div>
+    <div className={styles.breadcrumb}>
+      <div className={styles.item}>Tratamento de entregas</div>
+    </div>
+    <div className={styles.container}>
+      <div>
+        Pedido
+        <br />
+        {id}
+      </div>
+      <div>
+        Status do pedido
+        <br />
+        {status}
+      </div>
+      <div>
+        Entregas relacionadas
+        <br />
+        {Object.keys(fulfillments).map(fulfillment => (
+          <span className={styles.badge}>{fulfillment}</span>
+        ))}
+      </div>
+    </div>
   </header>
 );
 
-export default Header;
+export default () => {
+  const { data } = useSelector(({ pedido }) => pedido);
+  return Header(data);
+};
